e578cb6f3b6800c450f4a00c3ca5de28acd0b79c
khalil rahmouni <khalil.rahmouni13@gmail.com>
2022-03-31 21:54:14 +0200

    first branch

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        print("hello world");
+    }
+
+    private static void print(String s){
+        System.out.println(s);
     }
 }
+
+
